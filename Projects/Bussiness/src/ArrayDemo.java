0b8f9ec2b23e25627deaa412d4e8c6157bf95bb2
DanielMevs <dannymevs@hotmail.com>
2024-07-02 19:46:49 -0400

    completed variables declared with var and arrays sections

---
+++ Projects/Bussiness/src/ArrayDemo.java
@@ -0,0 +1,10 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        System.out.println(args.length);
+//        System.out.println(args[3]);
+
+        int[] nums = {10, 20, 30, 40, 50};
+        System.out.println(nums.length);
+        System.out.println(nums[1]);
+    }
+}
--- Projects/Bussiness/src/Person.java
+++ Projects/Bussiness/src/Person.java
@@ -12,6 +12,12 @@ public class Person {
     static private Company company2 = new Company();
     private NewsAgency agency;
 
+    public void myTestMethod() {
+        var age = 21;
+        var name = "Jake";
+
+    }
+
     public static void main(String[] args) {
         System.out.println(company1);
         System.out.println(company2);
