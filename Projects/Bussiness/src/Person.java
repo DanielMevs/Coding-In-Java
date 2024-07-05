d33e8e8ac043e70628dca30f5d4b469edace1896
DanielMevs <dannymevs@hotmail.com>
2024-07-04 20:53:09 -0400

    completed varargs section

--- Projects/Bussiness/src/Person.java
+++ Projects/Bussiness/src/Person.java
@@ -19,6 +19,16 @@ public class Person {
         return num1 + num2;
     }
 
+    public void test(int num1, int num2, String[] words) {
+        // do something clever with words
+    }
+
+    public void test2(int num1, int num2, String... words) {
+        // do soemthing clever
+        System.out.println(words[2]);
+        System.out.println(words.length);
+    }
+
     public static void main(String[] args) {
         Person p1 = new Person();
         p1.sayHello();
@@ -27,5 +37,8 @@ public class Person {
         char middleInitial = p1.getMiddleInitial();
         System.out.println(middleInitial);
         System.out.println(p1.add(5, 3));
+        String[] words = new String[] {"one", "two", "three"};
+        p1.test(3, 5, words);
+        p1.test2(3, 5, "one", "two", "three");
     }
 }
